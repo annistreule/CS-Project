6746b5ef2d136c12ef6ad213ed179eb6f7179837
annistreule <147740201+annistreule@users.noreply.github.com>
2023-11-29 14:36:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#Import relevant libraries
+# Import relevant libraries
 import streamlit as st
 from datetime import datetime, timedelta
 import pandas as pd
@@ -8,37 +8,18 @@ from collections import defaultdict
 if "inventory_list" not in st.session_state:
     st.session_state.inventory_list = []
 
-# Read the Product file
-df = pd.read_csv('products.csv')
+# File upload widget for the Product file
+uploaded_file = st.file_uploader("Upload your product file (CSV format)")
+if uploaded_file is not None:
+    # Read the uploaded file into a DataFrame
+    df = pd.read_csv(uploaded_file)
+else:
+    # Placeholder DataFrame or a message to prompt file upload
+    df = pd.DataFrame()
+    st.write("Please upload a CSV file.")
 
 # Initialize classes & subclasses 
-class Product:
-    def __init__(self, name, product_code, calories, expiry_days, quantity=1, owner=None):
-        self.name = name
-        self.product_code = product_code
-        self.calories = calories
-        self.expiry_days = expiry_days
-        self.quantity = quantity
-        self.owner = owner  # Added owner attribute
-
-class ApartmentFridge:
-    def __init__(self):
-        self.products = {}
-
-    def add_product(self, product):
-        if product.owner not in self.products:
-            self.products[product.owner] = []
-        if len(self.products[product.owner]) < 10:  # Assuming each fridge can hold up to 10 products
-            self.products[product.owner].append(product)
-            print(f"{product.name} added to the {product.owner}'s fridge.")
-        else:
-            print(f"The {product.owner}'s fridge is full!")
-
-    def display_fridge_contents(self):
-        for owner, products in self.products.items():
-            print(f"Owner: {owner}")
-            for product in products:
-                print(f" - {product.name} ({product.quantity} pieces)")
+# ... [Your existing class definitions]
 
 # ... [Rest of your existing code up to the end of streamlit buttons]
 
@@ -48,8 +29,8 @@ def save_to_excel(data, file_name='inventory.xlsx'):
     df.to_excel(file_name, index=False)
 
 # End of Streamlit app - call save_to_excel with inventory_list or other data
-save_to_excel(st.session_state.inventory_list)
+if st.button("Save Inventory to Excel"):
+    save_to_excel(st.session_state.inventory_list)
+    st.write("Inventory saved to Excel file.")
 
 # ... [Rest of your existing code]
-
-# Note: This code assumes that the rest of your Streamlit app and logic is correctly implemented.
