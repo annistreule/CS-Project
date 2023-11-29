a24499a1689ff1be86710fd5b8470cf4438f46e6
annistreule <147740201+annistreule@users.noreply.github.com>
2023-11-29 14:41:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,43 @@ from collections import defaultdict
 if "inventory_list" not in st.session_state:
     st.session_state.inventory_list = []
 
+# File uploader for the Product file
+uploaded_file = st.file_uploader("Upload your product file (CSV format)", type=["csv"])
+if uploaded_file is not None:
+    # Read the uploaded file into a DataFrame
+    df = pd.read_csv(uploaded_file)
+    st.session_state['product_df'] = df
+else:
+    df = pd.DataFrame(columns=['name', 'product_code', 'calories', 'expiry_days', 'quantity'])
+    st.session_state['product_df'] = df
+
+# Initialize classes & subclasses
+# [Your existing class definitions]
+
+# ... [Rest of your existing code]
+
+# Save data to an Excel file
+def save_to_excel(data, file_name='inventory.xlsx'):
+    df = pd.DataFrame(data)
+    df.to_excel(file_name, index=False)
+
+# Button to save the inventory list to an Excel file
+if st.button('Save Inventory to Excel'):
+    save_to_excel(st.session_state.inventory_list)
+    st.success('Inventory saved to Excel file.')
+
+# ... [Rest of your existing code]
+
+# Import relevant libraries
+import streamlit as st
+from datetime import datetime, timedelta
+import pandas as pd
+from collections import defaultdict
+
+# Initialize inventory list in session state to save data entries
+if "inventory_list" not in st.session_state:
+    st.session_state.inventory_list = []
+
 # File upload widget for the Product file
 uploaded_file = st.file_uploader("Upload your product file (CSV format)")
 if uploaded_file is not None:
