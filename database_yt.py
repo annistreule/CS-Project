14c7b7a7f70f8f3481903b86cd955ccf46e42c6f
annistreule <147740201+annistreule@users.noreply.github.com>
2023-12-02 19:28:07 +0100

    Update database_yt.py

--- database_yt.py
+++ database_yt.py
@@ -3,63 +3,34 @@ import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# Stellen Sie den Umfang der API ein
-scope = [
-    'https://spreadsheets.google.com/feeds',
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive.file',
-    'https://www.googleapis.com/auth/drive'
-]
-
-# Pfad zu Ihrer JSON-Anmeldedatei
-creds = ServiceAccountCredentials.from_json_keyfile_name('/Users/Anina/Desktop/CS/Project/Anina/CS-Project-main/projekt-cs-32b2fba1e1ff.json', scope)
-
-# Autorisieren Sie sich mit den Anmeldedaten
-client = gspread.authorize(creds)
-
-# Öffnen Sie das Google Sheet mit seinem Namen oder seiner ID
-sheet = client.open('Database_a').sheet1
+# ... (Der Teil mit den Credentials und dem Öffnen des Sheets bleibt gleich)
 
 # Dekorator für Caching
-@st.experimental_memo
-def load_data(sheet):
+@st.experimental_memo(ttl=60)  # Zeit in Sekunden, nach der der Cache erneuert wird
+def load_data():
     # Holen Sie sich alle Daten aus dem ersten Blatt Ihrer Tabelle als DataFrame
+    sheet = client.open('Database_a').sheet1
     data = pd.DataFrame(sheet.get_all_records())
     return data
 
-# Lädt die Daten
-data = load_data(sheet)
+data = load_data()
 
 # Zeigt die Daten in der Streamlit-App an
 st.write(data)
 
-# Felder für die Eingabe neuer Produktinformationen
-product_name = st.text_input('Product Name')
-expire_date = st.text_input('Expire Date')
+# ... (Der Teil mit der Eingabe und den Schaltflächen bleibt gleich)
 
-# Schaltfläche zum Hinzufügen neuer Daten
+# Anstelle von 'sheet.append_row([...])' und 'sheet.delete_row(row_count)'
+# werden Sie Ihren Code mit 'data = load_data()' nach jeder Änderung aktualisieren
+
 if st.button('Add Product'):
-    # Überprüfen Sie, ob die Felder ausgefüllt sind
-    if product_name and expire_date:
-        # Fügt die neuen Daten am Ende des Sheets hinzu
-        sheet.append_row([product_name, expire_date])
-        st.experimental_memo.clear() # Cache löschen, damit die Daten neu geladen werden
-        st.success('Product added successfully!')
-        data = load_data(sheet) # Daten neu laden
-        st.write(data) # Daten anzeigen
-    else:
-        st.error('Please fill out all fields')
+    # ... (Überprüfungslogik bleibt gleich)
+    sheet.append_row([product_name, expire_date])
+    data = load_data()  # Daten neu laden
+    st.write(data)  # Daten anzeigen
 
-# Schaltfläche zum Entfernen des letzten Produkts
 if st.button('Remove Last Product'):
-    # Anzahl der Zeilen im Sheet
-    row_count = len(data.index) + 1  # +1, weil sheet.get_all_records() die Kopfzeile nicht zählt
-    # Entfernt die letzte Zeile aus dem Sheet
-    if row_count > 1:  # Verhindert das Löschen der Kopfzeile
-        sheet.delete_row(row_count)
-        st.experimental_memo.clear() # Cache löschen, damit die Daten neu geladen werden
-        st.success('Last product removed successfully!')
-        data = load_data(sheet) # Daten neu laden
-        st.write(data) # Daten anzeigen
-    else:
-        st.error('No products to remove')
+    # ... (Logik zum Entfernen bleibt gleich)
+    sheet.delete_row(row_count)
+    data = load_data()  # Daten neu laden
+    st.write(data)  # Daten anzeigen
