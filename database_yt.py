5278c6b107c4aa28a6160ded3a2ed2d8393161d7
annistreule <147740201+annistreule@users.noreply.github.com>
2023-12-02 18:13:31 +0100

    Update database_yt.py

--- database_yt.py
+++ database_yt.py
@@ -1,6 +1,19 @@
 import streamlit as st
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-# This will write the text "Hello" on the web page.
-st.write('Hello')
+# Set up the connection to Google Sheets
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('path_to_your_credentials.json', scope)
+client = gspread.authorize(creds)
+
+# Open the Google Sheet
+sheet = client.open("your_google_sheet_name").sheet1
+
+# Get all the records of the data
+records = sheet.get_all_records()
+
+# Display the records in the Streamlit app
+st.write(records)
 
 
