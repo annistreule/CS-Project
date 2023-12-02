f6d7f87ec683cd32a40c2d2a16d04fcde7e4a7c5
annistreule <147740201+annistreule@users.noreply.github.com>
2023-12-02 19:24:15 +0100

    Update database_yt.py

--- database_yt.py
+++ database_yt.py
@@ -3,21 +3,24 @@ import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-key=AIzaSyCBWuNZ0lgRSw-bHFBh9-UIn3WazAkWrvA
-
 # Stellen Sie den Umfang der API ein
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+scope = [
+    'https://spreadsheets.google.com/feeds',
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive.file',
+    'https://www.googleapis.com/auth/drive'
+]
 
 # Pfad zu Ihrer JSON-Anmeldedatei
-creds = ServiceAccountCredentials.from_json_keyfile_name('path_to_your_credentials.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('/Users/Anina/Desktop/CS/Project/Anina/CS-Project-main/projekt-cs-32b2fba1e1ff.json', scope)
 
 # Autorisieren Sie sich mit den Anmeldedaten
 client = gspread.authorize(creds)
 
 # Öffnen Sie das Google Sheet mit seinem Namen oder seiner ID
-sheet = client.open('https://docs.google.com/spreadsheets/d/e/2PACX-1vQZ1UK4_05AyOBEslc9fbV1zs_7PD40YNJ22847f39SIgLOiKleT70C5HQ--uTjf3-8Afh3NMCG6NsH/pub?output=csv').sheet1
+sheet = client.open('Database_a').sheet1
 
-# New decorator for caching
+# Dekorator für Caching
 @st.experimental_memo
 def load_data(sheet):
     # Holen Sie sich alle Daten aus dem ersten Blatt Ihrer Tabelle als DataFrame
@@ -30,21 +33,33 @@ data = load_data(sheet)
 # Zeigt die Daten in der Streamlit-App an
 st.write(data)
 
-# Beispiel zum Hinzufügen neuer Daten zu Ihrem Google Sheet
-def add_data_to_sheet(new_data):
-    # Fügt eine neue Zeile am Ende des Sheets hinzu
-    sheet.append_row(new_data)
+# Felder für die Eingabe neuer Produktinformationen
+product_name = st.text_input('Product Name')
+expire_date = st.text_input('Expire Date')
 
-# Beispiel zum Entfernen von Daten aus Ihrem Google Sheet
-def remove_data_from_sheet(row_number):
-    # Entfernt eine Zeile aus dem Sheet
-    sheet.delete_row(row_number)
+# Schaltfläche zum Hinzufügen neuer Daten
+if st.button('Add Product'):
+    # Überprüfen Sie, ob die Felder ausgefüllt sind
+    if product_name and expire_date:
+        # Fügt die neuen Daten am Ende des Sheets hinzu
+        sheet.append_row([product_name, expire_date])
+        st.experimental_memo.clear() # Cache löschen, damit die Daten neu geladen werden
+        st.success('Product added successfully!')
+        data = load_data(sheet) # Daten neu laden
+        st.write(data) # Daten anzeigen
+    else:
+        st.error('Please fill out all fields')
 
-# Beispielhafte Nutzung der Funktionen
-if st.button('Add new data'):
-    add_data_to_sheet(['New Data', 'More Data', 'Another One'])
-    
-if st.button('Remove last row'):
-    all_data = sheet.get_all_values()
-    remove_data_from_sheet(len(all_data))
-
+# Schaltfläche zum Entfernen des letzten Produkts
+if st.button('Remove Last Product'):
+    # Anzahl der Zeilen im Sheet
+    row_count = len(data.index) + 1  # +1, weil sheet.get_all_records() die Kopfzeile nicht zählt
+    # Entfernt die letzte Zeile aus dem Sheet
+    if row_count > 1:  # Verhindert das Löschen der Kopfzeile
+        sheet.delete_row(row_count)
+        st.experimental_memo.clear() # Cache löschen, damit die Daten neu geladen werden
+        st.success('Last product removed successfully!')
+        data = load_data(sheet) # Daten neu laden
+        st.write(data) # Daten anzeigen
+    else:
+        st.error('No products to remove')
