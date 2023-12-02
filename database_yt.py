af42039576780d713eadd607dc141d3d0c5cf71b
annistreule <147740201+annistreule@users.noreply.github.com>
2023-12-02 17:50:11 +0100

    Update database_yt.py

--- database_yt.py
+++ database_yt.py
@@ -1,4 +1,4 @@
-
+# example/st_app.py
 
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
@@ -9,6 +9,5 @@ conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
 data = conn.read(spreadsheet=url, usecols=[0, 1])
 st.dataframe(data)
-
 
 
