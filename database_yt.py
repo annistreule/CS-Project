be008685f0a81f15689ebed8fb060fa66dd2c476
annistreule <147740201+annistreule@users.noreply.github.com>
2023-12-02 18:35:23 +0100

    Update database_yt.py

--- database_yt.py
+++ database_yt.py
@@ -1,15 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-# The URL of your public Google Sheet, make sure it's shared as "Anyone with the link can view"
-sheet_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQZ1UK4_05AyOBEslc9fbV1zs_7PD40YNJ22847f39SIgLOiKleT70C5HQ--uTjf3-8Afh3NMCG6NsH/pub?output=csv'
-
-# Use the pandas library to read the csv data from the Google Sheet
-@st.cache
+# New decorator for caching
+@st.experimental_memo
 def load_data(url):
     return pd.read_csv(url)
 
+# Your public Google Sheet URL here
+sheet_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQZ1UK4_05AyOBEslc9fbV1zs_7PD40YNJ22847f39SIgLOiKleT70C5HQ--uTjf3-8Afh3NMCG6NsH/pub?output=csv'
+
+# Load the data
 data = load_data(sheet_url)
 
-# Display the dataframe in the Streamlit app
+# Display the data in the Streamlit app
 st.write(data)
