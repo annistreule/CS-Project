69bfcfaa7afd51310e8113db110fa64d886c64a6
annistreule <147740201+annistreule@users.noreply.github.com>
2023-12-03 10:02:19 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,10 @@
+import streamlit as st
+
+# Diese Funktion wird ausgeführt, wenn die App läuft
+def main():
+    st.write("Anina")  # Dies schreibt "Anina" auf die Streamlit-Seite
+
+# Überprüft, ob dieses Skript direkt ausgeführt wird und nicht importiert wird
+if __name__ == "__main__":
+    main()
 
